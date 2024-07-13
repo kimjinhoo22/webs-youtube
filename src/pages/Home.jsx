a4dfd34f3518f7c6fafba34753b62b4e32bc3c42
jeanmia <wlsgnwkd22@naver.com>
2024-07-13 21:33:45 +0900

    10 페이지 SEO 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Main from '../components/section/Main'
 
 const Home = () => {
   return (
-    <div>Home</div>
+        <Main title="진후의 유튜브" description ="진후의 유튜브 채널 페이지 입니다.">
+            Home
+        </Main>
+    
   )
 }
 
