07721b8ff6b6f8f00d2c19d12cabb2321cfa5ac7
jeanmia <wlsgnwkd22@naver.com>
2024-07-18 14:42:27 +0900

    메인 섹션 나머지 콘텐츠 작업

--- src/components/contents/Gsap.jsx
+++ src/components/contents/Gsap.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { gsapText } from '../../data/gsap'
+import { Link } from 'react-router-dom'
 
 const Gsap = () => {
   return (
-    <div>Gsap</div>
+    <section id='website'>
+        <h2>GSAO 패럴랙스 효과를 하고 싶다면!</h2>
+        <div className="video__inner">
+            {gsapText.map((video, key)=>(
+                <div className="video" key={key}>
+                    <div className="video__thumb playIcon">
+                        <Link to={`/video/${video.videoId}`}>
+                            <img src={video.img} alt={video.title} />
+                        </Link>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </section>
   )
 }
 
--- src/components/contents/Youtube.jsx
+++ src/components/contents/Youtube.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { youtubeText } from '../../data/youtube'
 
 const Youtube = () => {
   return (
-    <div>Youtube</div>
+    <section id='website'>
+        <h2>지금 이 코딩을 영상으로</h2>
+        <div className="video__inner">
+            {youtubeText.map((video, key)=>(
+                <div className="video" key={key}>
+                    <div className="video__thumb playIcon">
+                        <Link to={`/video/${video.videoId}`}>
+                            <img src={video.img} alt={video.title} />
+                        </Link>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </section>
   )
 }
 
